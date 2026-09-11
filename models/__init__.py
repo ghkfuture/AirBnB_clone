31057b3a3a890e97bc0bf9facdaad74e9ff7e563
ghkfuture <ghkfuture@users.noreply.github.com>
2026-09-10 22:00:00 -0700

    Fix BaseModel kwargs initialization and __init__ storage binding

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Initialize the models package and create a unique FileStorage instance.
-"""
+"""Initialize storage for models package."""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-"""
-BaseModel class module.
-Defines all common attributes/methods for other classes.
-"""
+"""BaseModel module for AirBnB clone."""
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel:
-    """Defines common attributes and methods for future subclasses."""
+    """Base class for all models in the project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize attributes for BaseModel instance."""
+        """Initialize a new BaseModel instance."""
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ("created_at", "updated_at"):
-                        val = datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f"
-                        )
-                        setattr(self, key, val)
-                    else:
-                        setattr(self, key, value)
+                if key == "__class__":
+                    continue
+                elif key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.strptime(value, tformat))
+                else:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """Return official string representation of BaseModel instance."""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
-
     def save(self):
-        """Update updated_at with the current datetime and save instance."""
+        """Update updated_at and save to storage."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Return dictionary representation containing key-values of instance."""
-        res = self.__dict__.copy()
-        res["__class__"] = self.__class__.__name__
-        res["created_at"] = self.created_at.isoformat()
-        res["updated_at"] = self.updated_at.isoformat()
-        return res
+        """Return dictionary representation of instance."""
+        r_dict = self.__dict__.copy()
+        r_dict["__class__"] = self.__class__.__name__
+        r_dict["created_at"] = self.created_at.isoformat()
+        r_dict["updated_at"] = self.updated_at.isoformat()
+        return r_dict
+
+    def __str__(self):
+        """Return string representation."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
